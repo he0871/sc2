2dd349602e6709b4c9546b4f00b5c79990f714b3
he0871 <38711849+he0871@users.noreply.github.com>
2019-11-24 18:03:07 -0500

    first version

---
+++ client.py
@@ -0,0 +1,34 @@
+"""
+Description:fetch the match history via starcraft 2 community api
+Author: Jingyuan He
+Date: 11/24/2019
+"""
+import requests
+import json
+
+import credential
+
+url = "https://us.battle.net/oauth/token"
+
+payload = {
+    'grant_type': 'client_credentials',
+    'client_id': credential.client_id,
+    'client_secret': credential.client_secret,
+}
+response = requests.post(url, data=payload)
+print(response.content)
+print(response.text)
+print(type(response.content))
+res_dict = json.loads(response.content.decode('utf-8'))
+print(res_dict)
+print(res_dict["access_token"])
+access_token = res_dict["access_token"]
+print("match history")
+requests_url = "https://us.api.blizzard.com/sc2/legacy/profile/1/1/10400261/matches"
+Whole_url = requests_url + '?' + 'access_token=' + access_token
+MatchHistory = requests.get(Whole_url)
+print(MatchHistory)
+#print(MatchHistory.content)
+MH_dict = json.loads(MatchHistory.content.decode('utf-8'))
+matches = MH_dict["matches"]
+print(matches[0])
